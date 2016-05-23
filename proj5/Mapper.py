28d72d1dc24e9707a2575921022b0e62612201d2
juice500ml <juice500ml@gmail.com>
2016-05-23 15:43:23 +0900

    lower.

--- proj5/Mapper.py
+++ proj5/Mapper.py
@@ -3,7 +3,7 @@
 import sys
 
 for line in sys.stdin:
-    words = line.strip().split()
+    words = line.strip().lower().split()
     count = len(words)
     for i in range(count-1):
         print '%s %s\t1' % (words[i], words[i+1])
