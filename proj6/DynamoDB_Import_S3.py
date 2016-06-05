fb01139f5bb2e15123be4337804f7012a6d186ce
Kwanghee Choi <juice500ml@gmail.com>
2016-06-05 12:32:09 +0000

    parallelism

--- proj6/DynamoDB_Import_S3.py
+++ proj6/DynamoDB_Import_S3.py
@@ -1,15 +1,16 @@
+from threading import Thread
+
 from boto.s3.connection import S3Connection
 from boto.dynamodb2.table import Table
 from boto.dynamodb2.exceptions import ItemNotFound
 
 execfile('20141589.conf')
 
-s3 = S3Connection()
-bucket = s3.get_bucket('sp20141589-proj6')
-tbl = Table('proj6-20141589')
 
-for f in bucket.list():
-    ngram = f.get_contents_as_string().split('\n')
+def writeDB(ngram):
+    tbl = Table('project6')
+    ngram = ngram.split('\n')
+
     for elem in ngram:
         elem = elem.split('\t')
         if len(elem) == 2:
@@ -18,8 +19,14 @@ for f in bucket.list():
             except ValueError:
                 continue
             try:
-                item = tbl.get_item(BigramWord = elem[0])
-                item['BigramCount'] += elem[1]
+                item = tbl.get_item(words = elem[0])
+                item['counts'] += elem[1]
             except ItemNotFound:
-                tbl.put_item(data = {'BigramWord': elem[0], 'BigramCount': elem[1]})
-            
+                tbl.put_item(data = {'words': elem[0], 'counts': elem[1]})
+
+
+if __name__ == '__main__':
+    s3 = S3Connection()
+    bucket = s3.get_bucket('sp20141589-proj6')
+    for f in bucket.list():
+        Thread(target = writeDB, args=(f.get_contents_as_string(),)).start()
