f1b615c5a23be0b5f8474c157e41f61e9663637f
Kwanghee Choi <juice500ml@gmail.com>
2016-06-06 08:47:57 +0000

    Query done.

---
+++ proj6/DynamoDB_Query_CLI.py
@@ -0,0 +1,23 @@
+from boto.dynamodb2.table import Table
+from boto.dynamodb2.exceptions import ItemNotFound, ValidationException
+
+execfile('20141589.conf')
+
+myTable = Table('project6')
+while True:
+    try:
+        s = raw_input('Input word combination (Ctrl+D to exit) : ')
+    except EOFError:
+        print ''
+        break
+    ans = 0
+    try:
+        elem = myTable.get_item(words = s)
+        ans = elem['counts']
+    except ItemNotFound:
+        pass
+    except ValidationException:
+        print 'You have put empty string. Ignoring input.'
+        continue
+
+    print '%s has %d occurences.' % (s, ans)
